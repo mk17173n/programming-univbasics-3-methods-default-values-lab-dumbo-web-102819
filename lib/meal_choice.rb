460547098a2a5948c8c9d448319cac66db257e88
Monsur Khan <monsur.khan@gmail.com>
2019-10-22 05:04:39 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,6 @@
 # Your code her
-def meal_choice (veg1, veg2, protein="meat")
+def meal_choice(veg1, veg2, protein="meat")
 puts "What a nutritious meal!" 
-puts "A plate of #{meat} with #{veg1} and #{veg2}."
+puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
